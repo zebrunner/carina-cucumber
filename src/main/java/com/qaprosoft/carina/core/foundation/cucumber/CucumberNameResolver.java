fbe6f561f7ac63722734f4ec8344b6c3cfc87f34
Sergei Zagriychuk <sergeizagriychuk@gmail.com>
2020-10-08 00:21:06 +0300

    Data provider logic was removed from CucumberNameResolver

--- src/main/java/com/qaprosoft/carina/core/foundation/cucumber/CucumberNameResolver.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/cucumber/CucumberNameResolver.java
@@ -38,37 +38,7 @@ public class CucumberNameResolver implements TestNameResolver {
             name = result.getTestContext().getCurrentXmlTest().getName();
         }
 
-        if (result.getTestContext().getCurrentXmlTest().getAllParameters().containsKey(SpecialKeywords.EXCEL_DS_CUSTOM_PROVIDER) ||
-                result.getTestContext().getCurrentXmlTest().getAllParameters().containsKey(SpecialKeywords.DS_CUSTOM_PROVIDER)) {
-            // AUTO-274 "Passing status set on emailable report when a test step fails"
-            String methodUID = "";
-            for (int i = 0; i < result.getParameters().length; i++) {
-                if (result.getParameters()[i] != null) {
-                    if (result.getParameters()[i].toString().contains(SpecialKeywords.TUID + ":")) {
-                        methodUID = result.getParameters()[i].toString().replace(SpecialKeywords.TUID + ":", "");
-                        break; // first TUID: parameter is used
-                    }
-                }
-            }
-            if (!methodUID.isEmpty()) {
-                name = methodUID + " - " + name;
-            }
-        }
-
-        name = appendDataProviderLine(result, name);
-
         return name;
-    }
-
-    private static String appendDataProviderLine(ITestResult testResult, String testName) {
-        if (testResult.getMethod().isDataDriven() && testResult.getMethod().getDataProviderMethod().getMethod().getModifiers() > 1) {
-            // adding extra zero at the beginning of the data provider line number
-            int indexMaxLength = Integer.toString(testResult.getMethod().getDataProviderMethod().getMethod().getModifiers()).length() + 1;
-            String lineFormat = " [L%0" + indexMaxLength + "d]";
-            int index = testResult.getMethod().getParameterInvocationCount() + 1;
-            testName += String.format(lineFormat, index);
-        }
-        return testName;
     }
 
     public static String prepareTestName(String strFormat, PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
